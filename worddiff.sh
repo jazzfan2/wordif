cbbd19e741193a80fab8045047b10069958a71c0
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2025-12-10 14:30:42 +0100

    First commit

--- worddiff.sh
+++ worddiff.sh
@@ -11,7 +11,24 @@
 # - wdiff
 # - wkhtmltopdf (indien output naar pdf gewenst is)
 #
-################################################################################
+#####################################################################################
+#
+# Copyright (C) 2025 Rob Toscani <rob_toscani@yahoo.com>
+#
+# worddiff.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# worddiff.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+######################################################################################
 
 # Standaard output-formaat:
 format="html"
@@ -96,23 +113,3 @@ wdiff -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" "$1" "$2" |
 
 # En wegschrijven naar het gewenste formaat (default .html):
 cat <(echo "$html_intro") - <(echo "$html_coda") | store2file -
-
-
-#####################################################################################
-#
-# Copyright (C) 2025 Rob Toscani <rob_toscani@yahoo.com>
-#
-# worddiff.sh is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# worddiff.sh is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-######################################################################################
