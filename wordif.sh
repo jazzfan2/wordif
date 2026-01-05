f922bc788d0ccfeab291c3cf12d574408bea05af
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2026-01-06 00:38:38 +0100

    First commit

--- wordif.sh
+++ wordif.sh
@@ -199,11 +199,11 @@ pre {
 #       wdiff -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" \
 #                 <(sed "$esc_html" "$file1") <(sed "$esc_html" "$file2") |
 
-        wdiffer.py -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" \
-                       <(sed "$esc_html" "$file1") <(sed "$esc_html" "$file2") |
+#       wdiffer.py -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" \
+#                       <(sed "$esc_html" "$file1") <(sed "$esc_html" "$file2") |
 
-#       wdiffer.sh -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" \
-#                     <(sed "$esc_html" "$file1" | tr -d '\r' ) <(sed "$esc_html" "$file2" | tr -d '\r' ) |
+        wdiffer.sh -w "$delete_start" -x "$end" -y "$insert_start" -z "$end" \
+                      <(sed "$esc_html" "$file1") <(sed "$esc_html" "$file2") |
 
         # And save results in desired format (default HTML):
         cat <(echo "$html_intro") - <(echo "$html_coda") | store2file - $NUMBER
