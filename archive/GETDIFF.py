202b64999e6bb2f4bf3a79e235e730fe94365f5f
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2026-01-22 20:43:07 +0100

    Second commit

---
+++ archive/GETDIFF.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Naam: GETDIFF.py
+# Opgesteld: Rob Toscani
+# Datum: 21-01-2026
+# Beschrijving:
+# Voer het lokale programma 'wordif.sh' uit op de lokale directories 'oud/' en 'nieuw/'
+# ('knop'-onderdeel in de directory 'WORDIF')
+#
+#######################################################################################
+
+import os
+
+dirpath = os.path.dirname( __file__ )    # De locatie van dit python-script
+
+os.system('cd ' + dirpath + '; ./wordif.sh -d ./oud ./nieuw')
