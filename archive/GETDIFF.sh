c628d5bb7cb6f516954454fa512fb330b10da08b
jazzfan2 <149713903+jazzfan2@users.noreply.github.com>
2026-03-24 18:52:12 +0100

    Third commit

--- archive/GETDIFF.sh
+++ archive/GETDIFF.sh
@@ -14,8 +14,13 @@
 # Otherwise, if realpath -s is available (not on BSD-like systems!):
 # myloc="$(dirname "$(realpath -s $0)")"
 
-# Best solution:
-myloc="$(realpath "$(dirname $0)")"
+# Best solution, not needing the -s option:
+# myloc="$(realpath "$(dirname $0)")"
+
+# If realpath() isn't available at all, use Kusalananda's solution in:
+# https://unix.stackexchange.com/questions/558350/is-there-in-bash-a-builtin-command-to-get-the-absolute-path-of-a-relative-file
+myloc="$( ( OLDPWD=- CDPATH= cd -P -- "$(dirname $0)" && pwd ) )"
+
 cd "$myloc"
 
 ./wordif.sh -d oud nieuw
